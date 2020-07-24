244adef3d35b97937a24a29f18befb1f0dc82b25
Hope Gipson <gipson.hope@gmail.com>
2020-07-24 22:48:58 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,3 +7,5 @@ doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 doc.css(".headline-26OIBN").text.strip
 
 puts doc.css(".headline-26OIBN").text.strip
+
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
